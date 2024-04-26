973a3740796c9eec698b0dc975d221cc68787746
PETER <ptronning@gmail.com>
2024-04-25 19:10:23 -0700

    Hmm

--- CS100/CourseNotes/main.cpp
+++ CS100/CourseNotes/main.cpp
@@ -3,10 +3,66 @@
 //Write unit tests for area and perimeter functions
 #include <iostream>
 #include "circle.hpp"
+#include <cmath>
+# define M_PI 3.14159265358979323846
 using namespace std;
 int main(){
 Circle c(10);
 cout << "A circle of radius 10 has an area of " << c.area() << endl;
 cout << "A circle of radius 10 has a perimeter of " << c.perimeter() << endl;
 return 0;
+}
+TEST(DimensionSuite,AreaStdCase){
+    Circle test(2);
+    double result = test.area();
+    double testVal = 4 * M_PI;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,AreaLgCase){
+    Circle test(999);
+    double result = test.area();
+    double testVal = 999 * 999 * M_PI;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,AreaOneCase){
+    Circle test(1);
+    double result = test.area();
+    double testVal = M_PI;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,AreaZeroCase){
+    Circle test(0);
+    double result = test.area();
+    EXPECT_EQ(result,0);
+}
+TEST(DimensionSuite,AreaNegCase){
+    Circle test(-1);
+    EXPECT_DEATH(test.area(),"cannot have negative area");
+}
+TEST(DimensionSuite,PerimStdCase){
+    Circle test(2);
+    double result = test.perimeter();
+    double testVal = 2*M_PI*2;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,PerimLgCase){
+    Circle test(999);
+    double result = test.perimeter();
+    double testVal = 2*M_PI*999;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,PerimOneCase){
+    Circle test(1);
+    double result = test.perimeter();
+    double testVal = 2*M_PI;
+    EXPECT_EQ(result,testVal);
+}
+TEST(DimensionSuite,PerimZeroCase){
+    Circle test(0);
+    double result = test.perimeter();
+    EXPECT_EQ(result,0);
+}
+TEST(DimensionSuite,PerimNegCase){
+    Circle test(-1);
+    EXPECT_DEATH(test.perimeter(),"cannot have negative perimeter");
 }
